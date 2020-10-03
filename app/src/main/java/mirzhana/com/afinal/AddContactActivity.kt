6d04cc8e8821c97b37a2522514ea6c1efd3fcf10
AmanzholT <amanzhol.lw@gmail.com>
2020-10-03 17:54:18 +0600

    click listener improvements

--- app/src/main/java/mirzhana/com/afinal/AddContactActivity.kt
+++ app/src/main/java/mirzhana/com/afinal/AddContactActivity.kt
@@ -4,29 +4,37 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_add_contact.*
 import mirzhana.com.afinal.model.Contact
 
-class AddContactActivity : AppCompatActivity() {
+class AddContactActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_contact)
 
-        addBtn.setOnClickListener{
-            val name = nameEditText.text.toString()
-            val mobile = mobileEditText.text.toString()
-            val home = homeEditText.text.toString()
-            val work = workEditText.text.toString()
-            val group = groupEditText.text.toString()
-            val contact = Contact(0, name, mobile, home,work, group)
+        addBtn.setOnClickListener(this)
+    }
 
-            setResult(Activity.RESULT_OK, Intent().
-                    putExtra("contacts", contact))
-           // startActivityForResult(Intent(this, MainActivity::class.java), 1)
-            finish()
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.addBtn -> onAddButtonClicked()
         }
+    }
 
+    private fun onAddButtonClicked() {
+        val name = nameEditText.text.toString()
+        val mobile = mobileEditText.text.toString()
+        val home = homeEditText.text.toString()
+        val work = workEditText.text.toString()
+        val group = groupEditText.text.toString()
+        val contact = Contact(0, name, mobile, home,work, group)
 
+        setResult(
+                Activity.RESULT_OK,
+                Intent().putExtra("contacts", contact)
+        )
+        finish()
     }
 }
